3b5576c7a341e8374944094e9f0eebded2b461db
Pouya <pouyajoon@gmail.com>
2014-10-04 09:46:31 +0200

    update path & bin

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,14 @@
+
+if Rails.env.staging? || Rails.env.production?
+  exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+else
+  # exe_path = 'Rails.root.join('bin', 'wkhtmltopdf').to_s'
+  # exe_path = '/usr/local/bin/wkhtmltopdf'
+  exe_path = "/Users/pouya/.rvm/gems/ruby-2.1.1/bin/wkhtmltopdf"
+end
+
 WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => exe_path
 }
