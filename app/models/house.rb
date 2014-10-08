302a8a478d11c2369ff42db4cbdb37153ebfabd7
Pouya <pouyajoon@gmail.com>
2014-10-08 11:25:06 +0200

    update formated float

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,11 @@
 class House < ActiveRecord::Base
+  def rent_formatted
+    "%0.02f €" % self.rent
+  end
+  def apartment_costs_formatted
+    "%0.02f €" % self.apartment_costs
+  end
+  def total_rent
+    "%0.02f €" % (self.rent + self.apartment_costs)
+  end
 end
